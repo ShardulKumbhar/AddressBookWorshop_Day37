8cc27c918f3ba133c3770ae736f6c6637f96457f
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-04-01 08:29:48 +0530

    created person  class

---
+++ src/main/java/addressbookworkshop/AddressBook.java
@@ -0,0 +1,11 @@
+package addressbookworkshop;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AddressBook {
+	
+	public static void main(String[] args) {
+		System.out.println("Well Come to Address Book Workshop");
+	}
+}
---
+++ src/main/java/addressbookworkshop/Person.java
@@ -0,0 +1,92 @@
+package addressbookworkshop;
+
+public class Person {
+	/*
+	 * created variables
+	 */
+	public String firstName;
+	public String lastName;
+	public String address;
+	public String city;
+	public String state;
+	public String zip;
+	public String phno;
+	public String emailId;
+
+	/*
+	 * created getter and setters
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZip() {
+		return zip;
+	}
+
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	public String getPhno() {
+		return phno;
+	}
+
+	public void setPhno(String phno) {
+		this.phno = phno;
+	}
+
+	public String getEmailId() {
+		return emailId;
+	}
+
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+
+	/*
+	 * created to print or display
+	 */
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city=" + city
+				+ ", state=" + state + ", zip=" + zip + ", phno=" + phno + ", emailId=" + emailId + "]";
+	}
+
+}
